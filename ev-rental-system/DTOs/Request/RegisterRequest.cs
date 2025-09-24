983255102e0cc1f1ac05b069f3bcc1062d258cef
nphphuc <pavilionforc@gmail.com>
2025-09-25 04:20:51 +0700

    Remove useless folders

--- ev-rental-system/DTOs/Request/RegisterRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ev_rental_system.DTOs.Request
-{
-    public class RegisterRequest
-    {
-        public string FullName { get; set; }
-        public string Email { get; set; }
-        public string Password { get; set; }
-    }
-}
