e01ad6b0cdeb7169a7155b3ff4f2c44dd6ad5a23
nphphuc <pavilionforc@gmail.com>
2025-09-27 03:06:13 +0700

    fix authorizing roles, add managing roles (crud) for admin & staff

---
+++ APIs/DTOs/Response/LoginResponse.cs
@@ -0,0 +1,10 @@
+﻿namespace APIs.DTOs.Response
+{
+    public class LoginResponse
+    {
+        public string Token { get; set; }
+        public string Email { get; set; }
+        public string UserName { get; set; }
+        public IList<string> Roles { get; set; }
+    }
+}
