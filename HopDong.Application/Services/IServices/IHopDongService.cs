d3faedb761b679f34995c766faceef35b0313c19
nphphuc <pavilionforc@gmail.com>
2025-10-18 15:25:18 +0700

    test-contract

---
+++ HopDong.Application/Services/IServices/IHopDongService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HopDong.Application.Services.IServices
+{
+    public interface IHopDongService
+    {
+        Task<Guid> LuuHopDongVaTaoFileAsync(TaoHopDongDto request);
+        Task GuiEmailXacNhanAsync(Guid hopDongId, string email);
+        Task<HopDongXacNhanDto> LayHopDongDeXacNhanAsync(string token);
+        Task XacNhanKyHopDongAsync(string token);
+        Task XoaMemHopDongAsync(Guid id);
+    }
+}
