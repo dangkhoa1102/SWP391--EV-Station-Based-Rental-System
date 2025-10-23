87ae2e859501b81ce0883386ddb42daa46b6a91e
Le Tan Duc Thinh <scottvnyt@gmail.com>
2025-10-23 14:48:45 +0700

    Update Booking history UI

--- FE/src/App.jsx
+++ FE/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter, Routes, Route, useLocation } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UserProfile from './user/page/UserProfile'
 import HomePage from './user/page/HomePage'
 import CarListPage from './user/page/CarListPage'
@@ -11,15 +11,11 @@ import TestApi from './user/page/TestApi'
 
 import Header from './components/Header'
 import Footer from './components/Footer'
-import StaffPage from './staff/page/StaffPage'
-import AdminPage from './admin/page/AdminPage'
 
-function RoutedAppFrame() {
-  const location = useLocation();
-  const hideFrame = location.pathname.startsWith('/staff') || location.pathname.startsWith('/admin');
+export default function App(){
   return (
-    <>
-      {!hideFrame && <Header />}
+    <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/cars" element={<CarListPage />} />
@@ -29,20 +25,8 @@ function RoutedAppFrame() {
         <Route path="/booking-history" element={<BookingHistory />} />
         <Route path="/profile" element={<UserProfile />} />
         <Route path="/test-api" element={<TestApi />} />
-
-        {/* Backoffice routes */}
-        <Route path="/staff" element={<StaffPage />} />
-        <Route path="/admin" element={<AdminPage />} />
       </Routes>
-      {!hideFrame && <Footer />}
-    </>
-  );
-}
-
-export default function App(){
-  return (
-    <BrowserRouter>
-      <RoutedAppFrame />
+      <Footer />
     </BrowserRouter>
   )
 }
