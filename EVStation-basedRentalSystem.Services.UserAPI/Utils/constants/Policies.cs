1215c352863c673757a4746f76499fbc3739512b
nphphuc <pavilionforc@gmail.com>
2025-11-01 12:01:05 +0700

    remove microservice version

--- EVStation-basedRentalSystem.Services.UserAPI/Utils/constants/Policies.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace EVStation_basedRentalSystem.Services.UserAPI.Utils.constants
-{
-    public static class Policies
-    {
-        public const string RequireAdmin = "RequireAdmin";
-        public const string RequireManager = "RequireManager";
-    }
-}
--- EVStation-basedRentalSystem.Services.UserAPI/Utils/constants/Roles.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace EVStation_basedRentalSystem.Services.UserAPI.Utils.constants
-{
-    public static class Roles
-    {
-        public const string Admin = "Admin";
-        public const string Manager = "Manager";
-        public const string Customer = "Customer";
-    }
-}
