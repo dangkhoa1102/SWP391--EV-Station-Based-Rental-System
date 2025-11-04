fc2012ffb5577062af6ace4a2d8699423d45f21c
nphphuc <pavilionforc@gmail.com>
2025-11-04 11:14:28 +0700

    add attribute 'seats' into object Car and fix some error of contract's FE

--- Monolithic/Services/Interfaces/IBookingService.cs
+++ Monolithic/Services/Interfaces/IBookingService.cs
@@ -7,11 +7,11 @@ namespace Monolithic.Services.Interfaces
     {
         // Main booking flow methods (New Flow)
         Task<ResponseDto<BookingDto>> CreateBookingAsync(string userId, CreateBookingDto request);
-        
+
         Task<ResponseDto<BookingDto>> CheckInWithContractAsync(CheckInWithContractDto request);
         Task<ResponseDto<BookingDto>> CheckOutBookingAsync(CheckOutWithPaymentDto request);
-        
-        
+
+
         // Booking management methods
         Task<ResponseDto<PaginationDto<BookingDto>>> GetBookingsAsync(PaginationRequestDto request);
 
@@ -20,7 +20,7 @@ namespace Monolithic.Services.Interfaces
         Task<ResponseDto<List<BookingDto>>> GetUserBookingsAsync(string userId);
         Task<ResponseDto<BookingDto>> UpdateBookingAsync(Guid id, UpdateBookingDto request);
         Task<ResponseDto<string>> CancelBookingAsync(Guid id, string userId, string? reason = null);
-        
+
         // Utility methods
         Task<ResponseDto<bool>> CheckCarAvailabilityAsync(CheckAvailabilityDto request);
         Task<ResponseDto<decimal>> CalculateBookingCostAsync(Guid carId, DateTime startTime, DateTime endTime);
